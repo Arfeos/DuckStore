907528eaf98f650b6c88ac3845214e5428c9deca
Arfeos <83002671+Arfeos@users.noreply.github.com>
2026-09-04 09:05:01 +0200

    fix: solve broken link

--- src/js/components.js
+++ src/js/components.js
@@ -10,7 +10,7 @@ class headerComponent extends HTMLElement{
                     <ul>
                         <li><a href="index.html">Início</a></li>
                         <li><a href="catalog.html">Catálogo</a></li>
-                        <li><a href="contacti.html">Contacto</a></li>
+                        <li><a href="contactos.html">Contacto</a></li>
                     </ul>
                 </nav>
                 <div class="header-actions">
